16175642cdfaf7829a56da5097c3137e32e5603f
VITALY ZHUKOV <54074833+zhuksa@users.noreply.github.com>
2019-08-18 02:00:36 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,36 +1,31 @@
-import requests #импорт библиотеки 'Request'-иммитатор http-запросов
-from bs4 import BeautifulSoup as bs # импорт -позволяет распарсить ответ полученный от сервера
+from requests import Session
+from bs4 import BeautifulSoup as bs
 
-headers = {'accept': '*/*',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'} #иммуляция введения браузера
+HEADERS = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                         'Chrome/76.0.3809.100 Safari/537.36'}
 
-base_url = 'https://lenta.ru/news/2019/08/10/ranen/' #ссылка откуда парсим
+BASE_URL = str(input('Введите ссылку на статью:_'))  #'https://lenta.ru/news/2019/08/10/ranen/'
 
-def lenta_parse(base_url, headrs): # создание функции
-    session = requests.Session() # создание сессии
-    request = session.get(base_url, headers=headers) #иммуляция открытия страницы в браузере
-    if request.status_code == 200:
-        soup = bs(request.content, 'html.parser') #результат работы BeautifulSoup, котороая в свою очередь принимает request.content- весь ответ который отправляет сервер, html.parser-встроенный парсер в python, который позволяет разбивать ответ сервера на блоки html страницы
-        h1 = soup.find ('h1').text
-        divs = soup.find_all ('div', attrs={'class':'b-text clearfix js-topic__text'})
-        #for div in divs:
-            #title = div.find('p').text
 
+def lenta_parse(BASE_URL, HEADERS):
+    req = Session().get(BASE_URL, headers=HEADERS)
+    if req.status_code == 200:
+        soup = bs(req.content, 'html.parser')
+        h1 = soup.find('h1').text
+        print(h1.center(150))
+        all_p = soup.findAll('p')
+        for p in all_p:
+            pizdez = p.get_text()
+            print(pizdez)
         file = open('text.txt', 'w')
-
         print(file.name)
-        file.write(h1)
+        file.write(h1.center(200) + '\n')
+        for p in all_p:
+            file.write(p.get_text() + '\n')
         file.close()
-
-        print(file.closed)
-
-
-
-        print (h1.center(150))
-        print(divs)
     else:
-        print ('ERROR')
+        print('ERROR')
 
 
-
-lenta_parse (base_url, headers)
+lenta_parse(BASE_URL, HEADERS)
