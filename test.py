da819a98c489291e97546c525c7e039aaae40c00
VITALY ZHUKOV <zhuk.v.s93@gmail.com>
2019-09-05 00:02:34 +0500

    add class

--- test.py
+++ test.py
@@ -1,33 +1,51 @@
-from requests import Session
-from bs4 import BeautifulSoup as bs
-
-HEADERS = {'accept': '*/*',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                         'Chrome/76.0.3809.100 Safari/537.36'}
-
-  #'https://lenta.ru/news/2019/08/10/ranen/'
+import requests
+from bs4 import BeautifulSoup
 
 
-def lenta_parse(HEADERS):
-    print('Введите ссылку: ')
-    base_url = input()
-    req = Session().get(base_url, headers=HEADERS)
-    if req.status_code == 200:
-        soup = bs(req.content, 'html.parser')
+# https://lenta.ru/news/2019/08/10/ranen/
+
+class Article(object):
+    def __init__(self, h, p):
+        self.h = h
+        self.p = p
+
+    def get_text(self):
+        return self.h + self.p
+
+
+class Parser(object):
+    def __init__(self, url):
+        self.url = url
+        print(self.url)
+
+    def parse(self):
+        soup = BeautifulSoup(requests.get(self.url).content, 'html.parser')
         h1 = soup.find('h1').text
-        print(h1.center(150))
-        all_p = soup.findAll('p')
-        for p in all_p:
-            pizdez = p.get_text()
-            print(pizdez)
-        file = open('text.txt', 'w')
-        print(file.name)
-        file.write(h1.center(200) + '\n')
-        for p in all_p:
-            file.write(p.get_text() + '\n')
+        paragraphs = ''
+        for p in soup.findAll('p'):
+            for a in p.findAll('a'):
+                a.replace_with(p.a.string + ' URL:[' + p.a.get('href') + ']')
+            paragraphs = paragraphs + p.get_text() + '\n\n'
+        article = Article(h1.center(200) + '\n', paragraphs)
+        print(paragraphs)
+        file = File(h1 + '.txt')
+        file.save(article.get_text())
+
+
+class File(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def save(self, text):
+        file = open(self.file_name, 'w+')
+        file.write(text)
         file.close()
-    else:
-        print('ERROR')
 
 
-lenta_parse(HEADERS)
+class Dictionary(object):
+    def __init__(self, template):
+        self.template = {'width': 80, "indent": 2, 'url': 'True'}
+
+
+my_parser = Parser(input())
+my_parser.parse()
