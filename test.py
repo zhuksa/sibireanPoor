10ec3dc725795faf9f04bb40b012b37e926856c7
VITALY ZHUKOV <zhuk.v.s93@gmail.com>
2019-09-08 01:45:52 +0500

    'done'

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from json import loads
+from textwrap import fill
+from os import makedirs
 
-
-# https://lenta.ru/news/2019/08/10/ranen/
 
 class Article(object):
     def __init__(self, h, p):
@@ -16,35 +17,41 @@ class Article(object):
 class Parser(object):
     def __init__(self, url):
         self.url = url
-        print(self.url)
+        self.conf = Conf()
 
     def parse(self):
         soup = BeautifulSoup(requests.get(self.url).content, 'html.parser')
         h1 = soup.find('h1').text
-        paragraphs = ''
+        paragraphs = []
         for p in soup.findAll('p'):
             for a in p.findAll('a'):
                 a.replace_with(p.a.string + ' URL:[' + p.a.get('href') + ']')
-            paragraphs = paragraphs + p.get_text() + '\n\n'
-        article = Article(h1.center(200) + '\n', paragraphs)
-        print(paragraphs)
-        file = File(h1 + '.txt')
+            paragraphs.append(fill(p.get_text(), self.conf.width) + '\n' * self.conf.indent)
+        article = Article(h1.center(80) + '\n', ''.join(paragraphs))
+        file = File(h1 + '.txt', self.url)
         file.save(article.get_text())
 
 
 class File(object):
-    def __init__(self, file_name):
+    def __init__(self, file_name, url):
         self.file_name = file_name
+        self.url = url
 
     def save(self, text):
-        file = open(self.file_name, 'w+')
+        file_path = self.url.lstrip('https://').lstrip('http://')
+        makedirs(file_path)
+        file = open(file_path + 'index.txt', 'w+')
         file.write(text)
         file.close()
 
 
-class Dictionary(object):
-    def __init__(self, template):
-        self.template = {'width': 80, "indent": 2, 'url': 'True'}
+class Conf(object):
+    def __init__(self):
+        file = open('settings.txt', 'r')
+        conf = loads(str(file.read()))
+        self.width = conf['width']
+        self.indent = conf['indent']
+        file.close()
 
 
 my_parser = Parser(input())
